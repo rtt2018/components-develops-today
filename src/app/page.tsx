23b937682077df51181175752afc40fad853b8fa
Taras <r.taras.t.1@gmail.com>
2025-10-30 20:09:46 +0200

    upd: toast can receive settings from props or from a custom event

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,11 +12,20 @@ export default function Home() {
         <Input type="text" clearable={false} />
         <Input type="password" clearable={true} />
         <Input type="password" clearable={false} />
-        <button type="button" className={styles.showToast} onClick={() => window.dispatchEvent(new CustomEvent('show-butiful-toast', { detail: { text: 'Hello!', delay: 5000, enableCloseButton: true } }))}>Show toast delay: 5000 transition: slide enableCloseButton: true</button>
-        <button type="button" className={styles.showToast} onClick={() => window.dispatchEvent(new CustomEvent('show-butiful-toast', { detail: { text: 'Hello!', delay: 5000, enableCloseButton: false } }))}>Show toast delay: 5000 transition: slide enableCloseButton: false </button>
-        <button type="button" className={styles.showToast} onClick={() => window.dispatchEvent(new CustomEvent('show-butiful-toast', { detail: { text: 'Hello!', delay: 5000, enableCloseButton: true } }))}>Show toast delay: 5000 transition: slide enableCloseButton: true</button>
-        <button type="button" className={styles.showToast} onClick={() => window.dispatchEvent(new CustomEvent('show-butiful-toast', { detail: { text: 'Hello!', delay: 5000, enableCloseButton: true } }))}>Show toast delay: 5000 transition: slide enableCloseButton: true</button>
-        <Toast text="Hello!" primaryData="event" />
+
+        <button type="button" className={styles.showToast} onClick={() => window.dispatchEvent(new CustomEvent('show-butiful-toast', { detail: { text: 'Hello!', delay: 4000, enableCloseButton: true, transition: 'fade', eventName: "show-butiful-toast" } }))}>Show toast delay: 5000 transition: fade enableCloseButton: true</button>
+        <button type="button" className={styles.showToast} onClick={() => window.dispatchEvent(new CustomEvent('show-butiful-toast1', { detail: { text: 'Hello!', delay: 2000, enableCloseButton: false, transition: 'slide', eventName: "show-butiful-toast1" } }))}>Show toast delay: 2000 transition: slide enableCloseButton: false</button>
+        <button type="button" className={styles.showToast} onClick={() => window.dispatchEvent(new CustomEvent('showPropToast', { detail: { text: 'Hello!', delay: 3000, enableCloseButton: false, eventName: "showPropToast" } }))}>Show toast delay: 3000 transition: slide enableCloseButton: false </button>
+        <button type="button" className={styles.showToast} onClick={() => window.dispatchEvent(new CustomEvent('showPropToast1', { detail: { text: 'Hello!', delay: 5000, enableCloseButton: true, transition: 'fade' } }))}>Show toast delay: 5000 transition: slide enableCloseButton: true</button>
+        <button type="button" className={styles.showToast} onClick={() => window.dispatchEvent(new CustomEvent('showPropToast2', { detail: { text: 'Hello!', delay: 5000, enableCloseButton: true, transition: 'fade' } }))}>Show toast delay: 5000 transition: slide enableCloseButton: true</button>
+
+        <Toast text="Hello!" primaryData="event" eventName="show-butiful-toast" />
+        <Toast text="Hello, event!" primaryData="event" eventName="show-butiful-toast1" enableCloseButton={false} />
+
+        <Toast text="Hello!!" primaryData="props" eventName="showPropToast" delay={1000} enableCloseButton={false} transition='slide' />
+        <Toast text="Hello!!" primaryData="props" eventName="showPropToast1" delay={2000} enableCloseButton={true} transition='fade' />
+        <Toast text="Hello!!!" primaryData="props" eventName="showPropToast2" delay={1000} enableCloseButton={false} transition='slide' />
+
       </main>
     </div>
   );
--- src/components/Toast/Toast.tsx
+++ src/components/Toast/Toast.tsx
@@ -1,3 +1,18 @@
+/*
+Виклик Тоста: через кастомний евент, у який також можна передававти налаштування для Тоста
+window.dispatchEvent(new CustomEvent('event-name', { detail: { text: 'Hello!', delay: 2000, enableCloseButton: false, transition: 'slide', eventName: "show-butiful-toast1" } }))
+
+Пропси: 
+text = "Hello!", - текст повідомлення
+delay, - думаю, зрозуміло
+transition = 'slide', - перехід при появі і зникненні Тоста
+enableCloseButton = true, - активація кнопки закриття; true - кнопка є
+primaryData = 'props', - звідки брати налаштування: з пропсів, чи з евенту. З евенту беруться всі пропси, крім  eventName, primaryData
+eventName = 'show-butiful-toast' - назва евенту, при якому буде показано Тост, довільне, головнге передати однакові у пропс і диспатч евенту
+
+Потрібне покращення: демонтування з ДОМ після зникнення і перед показом
+*/
+
 'use client';
 import { useEffect, useState } from 'react';
 import css from './Toast.module.css'
@@ -7,44 +22,50 @@ import closeIcon from '@/img/clear.svg'
 
 type ToastTypes = {
     text: string,
-    delay?: number,
-    transition?: 'fade' | 'slide',
+    delay?: number | null,
+    transition?: 'fade' | 'slide' | '',
     enableCloseButton?: boolean,
-    primaryData?: 'props' | 'event'
+    primaryData?: 'props' | 'event' | ''
+    eventName: string
 }
 
-export default function Toast({ text = "Hello!", delay, transition = 'fade', enableCloseButton = true, primaryData = 'props'
-}: ToastTypes) {
+
+export default function Toast({ text = "Hello!", delay, transition = 'slide', enableCloseButton = true, primaryData = 'props', eventName = 'show-butiful-toast' }: ToastTypes) {
     const [hideToast, setHideToast] = useState(true)
-    const [propsData, setPropsData] = useState<ToastTypes>({
-        text,
-        delay,
-        transition,
-        enableCloseButton,
-        primaryData
+    const [propsData, setPropsData] = useState({
+        text, delay, transition, enableCloseButton, primaryData, eventName
     })
+    const handleHideToast = () => {
+        setHideToast(true)
+    }
 
     useEffect(() => {
         const showToast = (evt: Event) => {
             const customEvt = evt as CustomEvent<ToastTypes>
             if (primaryData === 'event') {
-                setPropsData((prevData) => { return { ...prevData, ...customEvt.detail } })
+                setPropsData((prevData) => { return { ...prevData, ...customEvt.detail, eventName, primaryData } })
             }
             setHideToast(false)
         }
-        window.addEventListener('show-butiful-toast', showToast)
-        return () => window.removeEventListener('show-butiful-toast', showToast)
-    }, [primaryData])
+        window.addEventListener(eventName, showToast)
+        return () => window.removeEventListener(eventName, showToast)
+    }, [text,
+        delay,
+        transition,
+        enableCloseButton,
+        primaryData,
+        eventName])
 
     useEffect(() => {
         if (typeof propsData.delay === "number") {
             const hideToast = setTimeout(() => {
-                setHideToast(true)
+                handleHideToast()
             }, propsData.delay)
 
             return () => clearTimeout(hideToast)
         }
     }, [propsData.delay, hideToast])
+
 
     return (
         <div className={`${css.toastContainer} ${hideToast ? (propsData.transition === 'fade' ? css.fade : css.slide) : ''}`}>
@@ -53,7 +74,7 @@ export default function Toast({ text = "Hello!", delay, transition = 'fade', ena
                     {propsData.text}
                 </div>
             </div>
-            {enableCloseButton && <button type="button" aria-label='Close toast' className={css.closeButton} onClick={() => { setHideToast(true) }}><Image src={closeIcon} alt='Close toast icon' className={css.closeIcon} /></button>}
+            {enableCloseButton && <button type="button" aria-label='Close toast' className={css.closeButton} onClick={handleHideToast}><Image src={closeIcon} alt='Close toast icon' className={css.closeIcon} /></button>}
         </ div>
     )
 }
